fd58d750e0140493f56c21923999b952c64e41fa
moonfighting <moonfighting@hotmail.com>
2013-10-04 20:37:09 +0800

    hello

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"hello world"<<endl;
+	return 0;
+}
+
